54832f6a5e1165cd3366d481825eda33cf8f9270
Mohamed El-Zafarani <misha@MSI>
2023-10-20 20:18:38 +0200

    102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,51 +1,52 @@
 #include "sort.h"
 
 /**
- * counting_sort - sorts array of int using counting sort algo.
+ * counting_sort - Sorts an array of integers using counting sort algorithm.
  * @array: Array to be sorted.
- * @size: Size of array.
+ * @size: Size of the array.
  *
- * Return: void.
+ * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
-	int top = 0, i, *count = NULL;
-	int *output = malloc(sizeof(int) * size);
+	int max = 0, i;
+	int *count, *sorted_array;
 
-	if (!output)
+	if (size < 2)
 		return;
-	if (size <= 1)
-	{
-		free(output);
+
+	sorted_array = malloc(sizeof(int) * size);
+	if (!sorted_array)
 		return;
-	}
+
 	for (i = 0; i < (int)size; i++)
-	{
-		if (array[i] > top)
-			top = array[i];
-	}
-	count = malloc(sizeof(int) * (top + 1));
+		if (array[i] > max)
+			max = array[i];
 
+	count = calloc(sizeof(int), max + 1);
 	if (!count)
 	{
-		free(output);
+		free(sorted_array);
 		return;
 	}
-	for (i = 0; i <= top; i++)
-		count[i] = 0;
+
 	for (i = 0; i < (int)size; i++)
 		count[array[i]]++;
-	for (i = 1; i <= top; i++)
+
+	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
-	print_array(count, top + 1);
+
+	print_array(count, max + 1);
 
 	for (i = (int)size - 1; i >= 0; i--)
 	{
-		output[count[array[i]] - 1] = array[i];
+		sorted_array[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
+
 	for (i = 0; i < (int)size; i++)
-		array[i] = output[i];
-	free(output);
+		array[i] = sorted_array[i];
+
+	free(sorted_array);
 	free(count);
 }
