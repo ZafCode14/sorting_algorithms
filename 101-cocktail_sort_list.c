96a020c43115813e36f0059bbf1d19edd42b3cbd
Mohamed El-Zafarani <misha@MSI>
2023-10-20 11:40:10 +0200

    101

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -39,7 +39,7 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *check = *list, *first = NULL;
 	int swapped = 1;
 
-	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (swapped)
