8872f54daa1727f8e89e359e9b3929f663859c14
Lelaabk <lolabkari@gmail.com>
2023-10-20 02:52:56 -0700

    Counting sort and Notation fixed!

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,54 +9,44 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, top = 0, *count_array = NULL;
-	int *output_array = malloc(sizeof(int) * size);
-
-	if (!output_array)
+	if (size < 2)
 		return;
 
-	if (size <= 1)
-	{
-		free(output_array);
-		return;
-	}
+	int top = array[0];
 
-	for (i = 0; i < (int)size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		if (array[i] > top)
 			top = array[i];
 	}
 
-	count_array = malloc(sizeof(int) * (top + 1));
-	if (!count_array)
+	int *count = malloc(sizeof(int) * (top + 1));
+
+	if (!count)
+		return;
+
+	for (int i = 0; i <= top; i++)
+		count[i] = 0;
+	for (size_t i = 0; i < size; i++)
+		count[array[i]]++;
+	for (int i = 1; i <= top; i++)
+		count[i] += count[i - 1];
+
+	int *output = malloc(sizeof(int) * size);
+
+	if (!output)
 	{
-		free(output_array);
+		free(count);
 		return;
 	}
-
-	for (i = 0; i <= top; i++)
-		count_array[i] = 0;
-
-	for (i = 0; i <= top; i++)
-		count_array[i] = 0;
-
-	for (i = 0; i < (int)size; i++)
-		count_array[array[i]]++;
-
-	for (i = 1; i <= top; i++)
-		count_array[i] += count_array[i - 1];
-
-	print_array(count_array, top + 1);
-
-	for (i = (int)size - 1; i >= 0; i--)
+	for (int i = size - 1; i >= 0; i--)
 	{
-		output_array[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
 	}
+	for (size_t i = 0; i < size; i++)
+		array[i] = output[i];
 
-	for (i = 0; i < (int)size; i++)
-		array[i] = output_array[i];
-
-	free(output_array);
-	free(count_array);
+	free(count);
+	free(output);
 }
