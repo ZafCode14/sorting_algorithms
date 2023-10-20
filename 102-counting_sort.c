4e3faa42113226c4c6e7c9c16765557070cc8d71
Lelaabk <lolabkari@gmail.com>
2023-10-20 03:00:44 -0700

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,44 +9,44 @@
  */
 void counting_sort(int *array, size_t size)
 {
+	int top = 0, i;
+	int *count = NULL, *output;
+
 	if (size < 2)
 		return;
 
-	int top = array[0];
-
-	for (size_t i = 0; i < size; i++)
-	{
-		if (array[i] > top)
-			top = array[i];
-	}
-
-	int *count = malloc(sizeof(int) * (top + 1));
-
-	if (!count)
-		return;
-
-	for (int i = 0; i <= top; i++)
-		count[i] = 0;
-	for (size_t i = 0; i < size; i++)
-		count[array[i]]++;
-	for (int i = 1; i <= top; i++)
-		count[i] += count[i - 1];
-
-	int *output = malloc(sizeof(int) * size);
+	output = malloc(sizeof(int) * size);
 
 	if (!output)
+		return;
+	for (i = 0; i < (int)size; i++)
+		top = array[i] > top ? array[i] : top;
+
+	count = malloc(sizeof(int) * (top + 1));
+	if (!count)
 	{
-		free(count);
+		free(output);
 		return;
 	}
-	for (int i = size - 1; i >= 0; i--)
+	for (i = 0; i <= top; i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= top; i++)
+		count_[i] += count[i - 1];
+
+	print_array(count, top + 1);
+
+	for (i = (int)size - 1; i >= 0; i--)
 	{
 		output[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 		array[i] = output[i];
 
-	free(count);
 	free(output);
+	free(count);
+}
+
 }
